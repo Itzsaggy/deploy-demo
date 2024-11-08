5557d5687164018a15ac44fadaddefb9f77eced3
Sagar Wankhede <111344899+Itzsaggy@users.noreply.github.com>
2024-11-08 09:05:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,47 @@
 const express = require('express');
 const connectDB = require('./db/dbConnection');
 const app = express();
-const port = 8000;
+
 const User = require('./db/user');
 const cors = require('cors');
-
-
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(express.json());
+app.use(cors());
 
-app.use(cors())
-
-app.post('/register',async(req,res)=>{
-    try{
-        const {username,password}=req.body;
-        console.log(req.body)
-        const user = new User({username,password});
+app.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        console.log(req.body);
+        const user = new User({ username, password });
         await user.save();
-        res.status(201).json({message:'Registration Succesful'})
+        res.status(201).json({ message: 'Registration Successful' });
+    } catch {
+        res.status(400).json({ error: 'Registration Failed' });
     }
-    catch{
-        res.status(400).json({error:'Registration Failed'})
-    }
-})
+});
 
-app.post('/login',async(req,res) => {
-    try{
-        const {username,password}=req.body;
-        const user = await User.findOne({username});
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
 
-        if(!user){
-            return res.status(401).json({error:'Invalid Username or Password'});
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid Username or Password' });
         }
-        if(user.password !== password){
-            return res.status(401).json({error:'Invalid Username or Password'});
+        if (user.password !== password) {
+            return res.status(401).json({ error: 'Invalid Username or Password' });
         }
         res.status(200).json({ message: 'Login Successful' });
-    }
-    catch(error){
+    } catch (error) {
         res.status(500).json({ message: 'Login Failed' });
     }
-})
+});
 
 connectDB();
 
-app.listen(port,()=>{
-    console.log("Server is listening...")
-})
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
